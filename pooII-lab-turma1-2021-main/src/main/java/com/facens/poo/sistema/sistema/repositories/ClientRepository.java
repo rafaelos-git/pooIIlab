4d3da2f0cca104dfb78c72c130f12567b002af3a
RafaelOliveiraSilva <rafael.oliveiratdm@outlook.com>
2021-03-23 20:07:30 -0300

    Paginando

--- pooII-lab-turma1-2021-main/src/main/java/com/facens/poo/sistema/sistema/repositories/ClientRepository.java
+++ pooII-lab-turma1-2021-main/src/main/java/com/facens/poo/sistema/sistema/repositories/ClientRepository.java
@@ -2,11 +2,17 @@ package com.facens.poo.sistema.sistema.repositories;
 
 import com.facens.poo.sistema.sistema.entities.Client;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository <Client,Long> {
+    //consulta JPQL
+    @Query("SELECT c FROM Client c")
+    public Page<Client> find(Pageable pageRequest);
     
 }
 
